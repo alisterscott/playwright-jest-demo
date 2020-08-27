0ed1502bac5fda0fdf3fb0fe519c359e26617247
Alister Scott <ascott@console.com.au>
2020-08-27 14:42:14 +1000

    Update example.spec.js

--- scenarios/example.spec.js
+++ scenarios/example.spec.js
@@ -10,3 +10,10 @@ test('can wait for an element to appear', async () => {
   await nav.visitHomePage(global.page)
   await global.page.waitForSelector('#elementappearschild', { visible: true, timeout: 5000 })
 }, jestTimeoutMS)
+
+test('can use an element that appears after on page load', async () => {
+  global.page = await pages.spawnPage()
+  await nav.visitHomePage(global.page)
+  const text = await global.page.textContent('#loadedchild')
+  expect(text).toBe('Loaded!')
+}, jestTimeoutMS)
